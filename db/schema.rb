086c36505915ae22e566a4f522199b1ec0668852
Ubuntu <ubuntu@ip-172-31-65-76.ec2.internal>
2022-09-14 08:55:36 +0000

    NameError (uninitialized constant TweetsController::Tweet):

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_14_064449) do
-
-  create_table "tweets", force: :cascade do |t|
-    t.string "message"
-    t.datetime "tdate"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
