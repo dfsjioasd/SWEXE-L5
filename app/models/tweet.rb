fd29a0077ffa85b92296a9d86e41dbcea1d5a0a0
Ubuntu <ubuntu@ip-172-31-65-76.ec2.internal>
2022-09-14 08:35:38 +0000

    ActiveRecord::PendingMigrationError (

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,2 @@
 class Tweet < ApplicationRecord
-    validates :message, presence: true
-    validates :message, length: {maximum: 140}
 end
--- db/migrate/20220914083451_create_tweets.rb
+++ db/migrate/20220914083451_create_tweets.rb
@@ -1,8 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-      t.string :message
-      t.datetime :tdate
+      t.string, :message
+      t.datetime, :tdate
+      t.binary :file
 
       t.timestamps
     end
