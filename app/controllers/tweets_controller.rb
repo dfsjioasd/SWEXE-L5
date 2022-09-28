91e6098e5565a3d64b87cfb20c2636fbb54a3c7a
Ubuntu <ubuntu@ip-172-31-65-76.ec2.internal>
2022-09-28 08:56:05 +0000

    NameError (undefined local variable or method `edit_tweets_path' for #<TweetsController:0x00007ff0cc31c5d0>

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -41,13 +41,13 @@ class TweetsController < ApplicationController
       if @tweets.update(message: params[:tweet][:message], tdate: @tweets.tdate, file: nil)
         redirect_to root_path
       else
-        render edit_tweet_path
+        render edit_tweets_path
       end
     else
       if @tweets.update(message: params[:tweet][:message], tdate: @tweets.tdate, file: params[:tweet][:file].read)
         redirect_to root_path
       else
-        render edit_tweet_path
+        render edit_tweets_path
       end
     end
   end
