96b3979350c9f5a37efe1bfcfabada9f865f8166
Ubuntu <ubuntu@ip-172-31-65-76.ec2.internal>
2022-09-28 08:35:40 +0000

    ActionController::RoutingError (No route matches [GET] "/tweets/edit/19"):

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :tweets
   root 'tweets#index'
+  resources :tweets
   get 'get_image/:id', to: 'tweets#get_image'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
