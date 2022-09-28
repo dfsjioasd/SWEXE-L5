78cce63a8b8cab8603029d187e0a4158c97e1efa
Ubuntu <ubuntu@ip-172-31-65-76.ec2.internal>
2022-09-28 08:34:28 +0000

    Completed 200 OK in 269ms (Views: 259.8ms | ActiveRecord: 0.7ms)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
+  resources :tweets
   root 'tweets#index'
-  get 'tweets/new', to: 'tweets#new'
-  post 'tweets', to: 'tweets#create'
-  delete 'tweets/:id', to: 'tweets#destroy'
-  get 'tweets/:id', to: 'tweets#show'
-  get 'tweets/edit/:id', to: 'tweets#edit'
-  patch 'tweets/:id', to: 'tweets#update'
+  
   get 'get_image/:id', to: 'tweets#get_image'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
