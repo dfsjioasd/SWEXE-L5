c266625e7c35251b347b94abaf615ab58caa5885
Ubuntu <ubuntu@ip-172-31-65-76.ec2.internal>
2022-10-12 07:56:57 +0000

    Completed 200 OK in 70ms (Views: 63.0ms | ActiveRecord: 0.9ms)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,17 @@ class UsersController < ApplicationController
       render new_tweet_path
     end
   end
+  
+  def login
+    if a = User.find_by(uid: params[:uid])
+        if BCrypt::Password.new(a.pass) == params[:pass]
+            session[:login_uid] = params[:uid]
+            redirect_to root_path
+        else
+            render "error"
+        end
+    else
+        render "error"
+    end
+  end
 end
