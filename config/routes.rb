60da4179037d987d3369f140acdbe6903d4caff9
Ubuntu <ubuntu@ip-172-31-65-76.ec2.internal>
2022-10-13 01:58:41 +0000

    Completed 200 OK in 230ms (Views: 221.9ms | ActiveRecord: 2.2ms)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'tweets#main'
   resources :tweets
   resources :users
-  resources :like
+  resources :likes
   get 'get_image/:id', to: 'tweets#get_image'
   post 'users/login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
